8902ffafaf3de0bce7b4a18b408a98f5660368bf
mike <myke_roly@hotmail.com>
2020-07-09 00:28:12 -0300

    fix merge

--- containers/footer/index.js
+++ containers/footer/index.js
@@ -6,12 +6,7 @@ import { ContextMP } from '../../context/MPContext';
 
 const Footer = () => {
   const contextMP = React.useContext(ContextMP);
-<<<<<<< HEAD
-  const { dataMP, getDat } = contextMP;
-=======
   const { getMethodsPayment } = contextMP;
->>>>>>> 412e4c530215dca7d5aaf9626fc7c3ef9ffebc33
-
   const yeard = new Date().getFullYear();
   return (
     <FooterWrapper>
@@ -22,11 +17,7 @@ const Footer = () => {
           <a href="https://www.facebook.com/" target="_blank" rel="noreferrer noopener" arial-label="link facebook"><IoLogoFacebook /><span>facebook</span></a>
         </FooterSocial>
         <p><small>myke_roly | {yeard} &copy; Derechos reservados</small></p>
-<<<<<<< HEAD
-        <button onClick={() => {console.log(dataMP); getDat() }}>get data</button>
-=======
         <button onClick={getMethodsPayment}>get data</button>
->>>>>>> 412e4c530215dca7d5aaf9626fc7c3ef9ffebc33
       </Container>
     </FooterWrapper>
   );
